c4f2c6de4c3d9cb9fda80248769119231048995c
Amitvar8 <152775621+Amitvar8@users.noreply.github.com>
2023-12-24 12:07:31 +0200

    Update shiur2.java

--- shiur2.java
+++ shiur2.java
@@ -1,4 +1,51 @@
-//1.
+package yudim;
 
+public class Shiur2 
+{
 
-//2.
+	public static void main(String[] args)
+	{
+	/*1	int x = 5;
+		int y = 6;
+		int b = 4;
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(b);
+	}  */
+	/*2	int x = 5;
+	      double y = 1.5;
+	      System.out.println(x+ "   "+ y)  ; 
+	      */
+//3   char h ='A';
+//	char g ='m';
+//	char n ='i';
+//	char y ='t';
+//	System.out.println(h+g+n+y);
+	
+	/*4   int x = 5;
+	int y = 4;
+	System.out.ptintln(x" bigger then" y );
+	*/
+	/*5    int x = 5;
+	int y = 6;
+	int z = 4;
+	System.out.ptintln(x+y+z/3);
+	*/
+	/*6    int x = 5;
+	int y = 4;
+	System.out.println(x*x+y*y);
+	*/
+	/*7    int x = 6;
+	int t = 2'
+	int v = 5
+	System.out.println(x*t+v);
+	*/
+	/*8    String x = “I like to study computer science”;
+	String y = “and therefore ready to invest”;
+	String z = “hours a day for practice”;
+	int hours = 5
+	System.out.println(x + " " + y + " " + hours + z);\
+	*/
+}
+	
+}
